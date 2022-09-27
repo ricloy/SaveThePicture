90a84a1db9256cc94c0b48c7e5f5c731d74cd170
Marco <marcxosm@gmail.com>
2022-09-27 19:46:22 +0200

    search in every comment and not only in the first one; minor fixes; add some stats

--- src/osmnotes_picturebackup.py
+++ src/osmnotes_picturebackup.py
@@ -11,11 +11,11 @@ from pathlib import Path
 api_url = "https://www.openstreetmap.org/api/0.6/notes"
 webarchive_url = "https://web.archive.org/save/"
 
-#Italy bbox
-min_lat=35.2889616   
-max_lat=47.0921462
-min_lon=6.6272658
-max_lon=18.7844746
+# Italy bbox
+min_lat = 35.2889616
+max_lat = 47.0921462
+min_lon = 6.6272658
+max_lon = 18.7844746
 
 # max retries
 MAX_RETRIES = 5
@@ -23,10 +23,18 @@ MAX_RETRIES = 5
 # wait time in seconds between each retry
 WAIT_TIME = 10
 
-steps=4
-for x in range(0,steps):
-    for y in range(0,steps):
-        print("Analysing bbox {}/{}…".format(x*steps + y + 1,steps*steps))
+# BBOX is divided in a steps x steps grid
+steps = 4
+
+# total counters
+total_notes = 0
+total_closed_notes = 0
+total_westnordost_links = 0
+total_saved_westnordost_links = 0
+
+for x in range(0, steps):
+    for y in range(0, steps):
+        print("Analysing bbox {}/{}…".format(x*steps + y + 1, steps * steps))
         bbox = (min_lon + x*(max_lon - min_lon)/steps, min_lat + y*(max_lat - min_lat)/steps, min_lon + (x+1)*(max_lon - min_lon)/steps, min_lat + (y+1)*(max_lat - min_lat)/steps)
         r = requests.get(api_url + "?bbox=" + ",".join([str(x) for x in bbox]) + "&limit=10000")
         retry = 0
@@ -38,24 +46,43 @@ for x in range(0,steps):
             print("Error: max number of retries ({}) reached; exiting…".format(MAX_RETRIES))
             sys.exit(-1)
         notes = etree.fromstring(r.content)
-    
+
+        # partial counters
+        part_notes = len(notes.getchildren())
+        part_closed_notes = 0
+        part_westnordost_links = 0
+        part_saved_westnordost_links = 0
+
         for note in notes.getchildren():
             if note.xpath('./status/text()')[0] == "closed":
+                part_closed_notes = part_closed_notes + 1
                 print("Inspecting closed note {}…".format(note.xpath("./id/text()")[0]))
-                first_comment = note.xpath('./comments/comment[1]/text/text()')[0]
-                westnordost_link = re.search(r'https://westnordost\.de/([a-z])/([0-9]+\.jpg)', first_comment)
-                if westnordost_link:
-                    if not exists("./data/{}/{}".format(westnordost_link.group(1), westnordost_link.group(2))):
-                        Path("./data/{}".format(westnordost_link.group(1))).mkdir(parents=True, exist_ok=True)
-                        with open("./data/{}/{}".format(westnordost_link.group(1), westnordost_link.group(2)),'w') as f:
-                            pass
-                        retry = 0
-                        print("Saving {} to {}".format(westnordost_link.group(1) + westnordost_link.group(2), (webarchive_url + westnordost_link.group(0))))
-                        r = requests.head(webarchive_url + westnordost_link.group(0))
-                        while (r is None or r.status_code != 302) and retry < MAX_RETRIES:
-                            sleep(WAIT_TIME)
-                            retry = retry + 1
+                comments = note.xpath('./comments/comment/text/text()')
+                for comment in comments:
+                    westnordost_link = re.search(r'https://westnordost\.de/([a-z])/([0-9]+\.jpg)', comment)
+                    if westnordost_link:
+                        part_westnordost_links = part_westnordost_links + 1
+                        if not exists("./data/{}/{}.idx".format(westnordost_link.group(1), westnordost_link.group(2))):
+                            Path("./data/{}".format(westnordost_link.group(1))).mkdir(parents=True, exist_ok=True)
+
+                            retry = 0
+                            print("Saving {} to {}".format(westnordost_link.group(1) + westnordost_link.group(2), (webarchive_url + westnordost_link.group(0))))
                             r = requests.head(webarchive_url + westnordost_link.group(0))
-                   	     
-        
-#r = requests.get()
+                            while (r is None or r.status_code != 302) and retry < MAX_RETRIES:
+                                sleep(WAIT_TIME)
+                                retry = retry + 1
+                                r = requests.head(webarchive_url + westnordost_link.group(0))
+                            if retry >= MAX_RETRIES:
+                                print("Error: max number of retries ({}) reached; exiting…".format(MAX_RETRIES))
+                                sys.exit(-2)
+
+                            with open("./data/{}/{}.idx".format(westnordost_link.group(1), westnordost_link.group(2)), 'w') as f:
+                                f.write(r.headers['location'] + "\n")
+                                part_saved_westnordost_links = part_saved_westnordost_links + 1
+        print("Partial stats: step notes: {}, step closed notes: {}, step westnordost links: {}, step saved westnordost links: {}".format(part_notes, part_closed_notes, part_westnordost_links, part_saved_westnordost_links))
+        total_notes = total_notes + 1
+        total_closed_notes = total_closed_notes + 1
+        total_westnordost_links = total_westnordost_links + 1
+        total_saved_westnordost_links = total_saved_westnordost_links + 1
+
+print("Final stats for this run: checked notes: closed notes: {}, total westnordost links: {}, total saved westnordost links: {}".format(total_notes, total_closed_notes, total_westnordost_links, total_saved_westnordost_links))
